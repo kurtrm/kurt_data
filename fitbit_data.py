38da38603d91ec79d5715ed760470bd7a3af1e1d
Kurt <kurtrm@gmail.com>
2017-10-10 10:41:37 -0700

    I think my refresh_cb function is working, I managed to call one of the class methods to get an activity list.

--- fitbit_data.py
+++ fitbit_data.py
@@ -10,18 +10,24 @@ REFRESH_TOKEN = os.environ['refresh_token']
 USER_ID = os.environ['user_id']
 
 
+def refresh_cb(token_dict):
+    """Function for refreshing access_token, refresh_token, and expires_at."""
+    os.environ['access_token'] = token_dict['access_token']
+    os.environ['expires_at'] = str(token_dict['expires_at'])
+    os.environ['refresh_token'] = token_dict['refresh_token']
+    # self.access_token = os.environ['access_token']
+    # self.refresh_token = os.environ['refresh_token']
+    # self.expires_at = os.environ['expires_at']
+
 kurt = Fitbit(
     CLIENT_ID,
     SECRET,
     access_token=ACCESS_TOKEN,
     refresh_token=REFRESH_TOKEN,
     expires_at=EXPIRES_AT,
+    refresh_cb=refresh_cb
 )
 
 #  Activate this project's environment.
 #  Run ../python-fitbit/gather_keys_oauth2.py $client_id $secret
 #  Set the new environmental variables from the output
-
-
-def refresh_cb(token_dict):
-    """Function for refreshing access_token, refresh_token, and expires_at."""
