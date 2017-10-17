2714117bcf519bbf17be2e78425ff236d0127882
Kurt <kurtrm@gmail.com>
2017-10-17 12:19:35 -0700

    You can't change environment variables through os, i'll need to workaround. Also, not much real progress, SO question outstanding.

--- fitbit_data.py
+++ fitbit_data.py
@@ -5,16 +5,26 @@ import os
 CLIENT_ID = os.environ['client_id']
 SECRET = os.environ['secret']
 ACCESS_TOKEN = os.environ['access_token']
-EXPIRES_AT = os.environ['expires_in']
+EXPIRES_AT = float(os.environ['expires_at'])
 REFRESH_TOKEN = os.environ['refresh_token']
 USER_ID = os.environ['user_id']
 
 
-def refresh_cb(token_dict):
+def refresh_cb(self, token_dict):
     """Function for refreshing access_token, refresh_token, and expires_at."""
-    os.environ['access_token'] = token_dict['access_token']
-    os.environ['expires_at'] = str(token_dict['expires_at'])
-    os.environ['refresh_token'] = token_dict['refresh_token']
+    # global ACCESS_TOKEN
+    # global EXPIRES_AT
+    # global REFRESH_TOKEN
+    # ACCESS_TOKEN = token_dict['access_token']
+    # EXPIRES_AT = str(token_dict['expires_at'])
+    # REFRESH_TOKEN = token_dict['refresh_token']
+    import pdb; pdb.set_trace()
+    self.access_token = self.token_dict['access_token']
+    self.expires_at = self.token_dict['expires_at']
+    self.refresh_token = self.token_dict['refresh_token']
+    # os.environ['access_token'] = ACCESS_TOKEN
+    # os.environ['expires_at'] = str(EXPIRES_AT)
+    # os.environ['refresh_token'] = REFRESH_TOKEN
 
     return token_dict
 
