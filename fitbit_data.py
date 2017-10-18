e8453d2699984644eb81a0037068f2e41186f315
Kurt <kurtrm@gmail.com>
2017-10-18 16:34:33 -0700

    This is a bet of a mess right now, but I think I have a way forward to get the tokens.

--- fitbit_data.py
+++ fitbit_data.py
@@ -1,40 +1,29 @@
 """Script for pulling data from the Fitbit API."""
 from fitbit import Fitbit
 import os
+import yaml
 
 CLIENT_ID = os.environ['client_id']
 SECRET = os.environ['secret']
-ACCESS_TOKEN = os.environ['access_token']
-EXPIRES_AT = float(os.environ['expires_at'])
-REFRESH_TOKEN = os.environ['refresh_token']
-USER_ID = os.environ['user_id']
 
 
 def refresh_cb(self, token_dict):
     """Function for refreshing access_token, refresh_token, and expires_at."""
-    # global ACCESS_TOKEN
-    # global EXPIRES_AT
-    # global REFRESH_TOKEN
-    # ACCESS_TOKEN = token_dict['access_token']
-    # EXPIRES_AT = str(token_dict['expires_at'])
-    # REFRESH_TOKEN = token_dict['refresh_token']
+    # with shelve.open('tokens.db', writeback=True) as tokens:
+    #     tokens['access_token'] = token_dict['access_token']
+    #     tokens['expires_at'] = token_dict['expires_at']
+    #     tokens['refresh_token'] = token_dict['refresh_token']
+
     import pdb; pdb.set_trace()
     self.access_token = self.token_dict['access_token']
     self.expires_at = self.token_dict['expires_at']
     self.refresh_token = self.token_dict['refresh_token']
-    # os.environ['access_token'] = ACCESS_TOKEN
-    # os.environ['expires_at'] = str(EXPIRES_AT)
-    # os.environ['refresh_token'] = REFRESH_TOKEN
 
     return token_dict
 
 kurt = Fitbit(
     CLIENT_ID,
-    SECRET,
-    access_token=ACCESS_TOKEN,
-    refresh_token=REFRESH_TOKEN,
-    expires_at=EXPIRES_AT,
-    refresh_cb=refresh_cb
+    SECRET
 )
 
 #  Activate this project's environment.
