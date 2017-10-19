b42bf475f48b8bc7f2a0d58584d06336a0054b25
Kurt <kurtrm@gmail.com>
2017-10-19 11:12:52 -0700

    Had to remove some files.

--- fitbit_data.py
+++ fitbit_data.py
@@ -1,30 +1,52 @@
 """Script for pulling data from the Fitbit API."""
 from fitbit import Fitbit
-import os
-import yaml
+# import os
+# import yaml
 
-CLIENT_ID = os.environ['client_id']
-SECRET = os.environ['secret']
+# CLIENT_ID = os.environ['client_id']
+# SECRET = os.environ['secret']
 
 
 def refresh_cb(self, token_dict):
     """Function for refreshing access_token, refresh_token, and expires_at."""
-    # with shelve.open('tokens.db', writeback=True) as tokens:
-    #     tokens['access_token'] = token_dict['access_token']
-    #     tokens['expires_at'] = token_dict['expires_at']
-    #     tokens['refresh_token'] = token_dict['refresh_token']
-
-    import pdb; pdb.set_trace()
     self.access_token = self.token_dict['access_token']
-    self.expires_at = self.token_dict['expires_at']
     self.refresh_token = self.token_dict['refresh_token']
+    self.expires_at = self.token_dict['expires_at']
 
     return token_dict
 
-kurt = Fitbit(
-    CLIENT_ID,
-    SECRET
-)
+# with open('config.yml') as config:
+#     tokens = yaml.load(config)
+
+
+def instantiate_fitbit_kurt():
+    """Uses gather_keys_oauth2 class to gather tokens and instantiate a
+
+    'Kurt' Fitbit class.
+    """
+    import gather_keys_oauth2
+    import os
+
+    CLIENT_ID = os.environ['client_id']
+    SECRET = os.environ['secret']
+    server = gather_keys_oauth2.OAuth2Server(
+        CLIENT_ID,
+        SECRET
+    )
+    server.browser_authorize()
+    import pdb; pdb.set_trace()
+    access_token = server.oauth.token['access_token']
+    refresh_token = server.oauth.token['refresh_token']
+    expires_at = server.oauth.token['expires_at']
+    kurt = Fitbit(
+        CLIENT_ID,
+        SECRET,
+        access_token=access_token,
+        refresh_token=refresh_token,
+        expires_at=expires_at,
+        refresh_cb=refresh_cb
+    )
+    return kurt
 
 #  Activate this project's environment.
 #  Run ../python-fitbit/gather_keys_oauth2.py $client_id $secret
