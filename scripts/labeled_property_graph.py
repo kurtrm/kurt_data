57787d5e430298d0636c08f24e69769eaa3cef00
Kurt <kurtrm@gmail.com>
2017-11-05 21:04:27 -0800

    Working on remove_relationship.

--- scripts/labeled_property_graph.py
+++ scripts/labeled_property_graph.py
@@ -117,8 +117,32 @@ class LabeledPropertyGraph:
             else:
                 self._relationships[name] = {node_a: {node_b: relationship}}
         else:
-            raise KeyError('Node not in graph')  # More description
-
+            raise KeyError('Node not in graph')
+    """  ================ add_relationship refactor ========================
+    def add_relationship(self, name, node_a, node_b):
+        Refactored add_relationship for EAFP.
+        nodes = self.nodes()
+        if self._relationship[name][node_a].get(node_b):
+            raise ValueError('Relationship already exists between nodes')
+        elif node_a in nodes or node_b in nodes:
+            raise KeyError('A node is not present in this graph')
+        try:
+            self._relationship[name][node_a][node_b] = Relationship(name)
+        except KeyError as key:
+            if key == name:
+                self._relationships[name] = {node_a: {node_b: Relationship(name)}}
+            elif key == node_a:
+                self._relationship[name][node_a] = {node_b: Relationship(name)}
+            else:
+                self._relationship[name][node_a][node_b] = Relationship(name)
+        try:
+            self._graph[node_a][node_b].append(name)
+        except AttributeError:
+            curr_rel = self._graph[node_a][node_b]
+            self._graph[node_a][node_b] = [curr_rel, name]
+        except KeyError:
+            self._graph[node_a][node_b] = name
+    ======================================================================== """
     def remove_relationship(self, name, node_a, node_b):
         """Remove a relationship between two nodes."""
         pass
