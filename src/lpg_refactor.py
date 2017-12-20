e1dafed1632b95ed27cd1576f0782cad627b99e4
kurt <kurtrm@gmail.com>
2017-12-19 16:11:17 -0800

    Working on the lpg now. Thought about it, and I put some restrictions on the __setitem__ method since I don't want the user to nefariously make relationships or nodes other types of objects.

--- src/lpg_refactor.py
+++ src/lpg_refactor.py
@@ -7,12 +7,12 @@ for simplicity. Particularly:
 
     Relationships:
         - __getitem__ to return properties
-        - Keys will now be a tuple. So lpg['Kurt', 'Melissa'] will return
-        the dictionary of relationships between the two nodes.
         - I thought briefly about throwing nodes and relationships in the same dict,
           but I'm going to keep these separate for now.
 
     LPG:
+        - Relationship keys will now be a tuple. So lpg['Kurt', 'Melissa'] will return
+        the dictionary of relationships between the two nodes.
         - Modify __getitem__ to return either nodes or relationships
         - Implement __iter__
         - Define a property method for size, as well as a setter
@@ -133,3 +133,32 @@ Properties (key: value)
         return self.__repr__()
 
 
+class LabeledPropertyGraph:
+    """Define a labeled property graph as dictionary composition."""
+
+    def __init__(self):
+        """Initialize the graph as a dictionary."""
+        self._graph = {}
+        self._nodes = {}
+        self._relationships = {}
+
+    def __getitem__(self, key):
+        """Return either node or relationship."""
+        if isinstance(key, tuple):
+            return self._relationships[key]
+        return self._nodes[key]
+
+    def __setitem__(self, key, item):
+        """Modify or create new nodes or relationships.
+           Warning: passing a tuple into the subscript will
+           create new relationship, not node."""
+        if isinstance(key, tuple):
+            if not isinstance(item, Relationship):
+                raise ValueError("Graph relationships must"
+                                 "be of type Relationship")
+            self._relationships[key] = item
+        else:
+            if not isinstance(item, Node):
+                raise ValueError("Graph nodes must"
+                                 "be of type Node")
+            self._nodes[key] = item
